df4749c61939456532c01f874435ac5e1e6bf615
tfitts <tfitts@gmail.com>
2016-08-12 16:27:09 -0600

    Allow integers for js_code date

--- lib/lazy_high_charts/options_key_filter.rb
+++ lib/lazy_high_charts/options_key_filter.rb
@@ -39,6 +39,7 @@ module LazyHighCharts
       end
 
       def date_to_js_code date
+        return date if date.is_a?(Integer)
         "Date.UTC(#{date.year}, #{date.month - 1}, #{date.day}, #{date.hour}, #{date.min}, #{date.sec})".js_code
       end
     end
